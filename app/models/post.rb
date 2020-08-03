7a45de71f671c884764c4331347d3cc815219210
jessegan <jesse13579@gmail.com>
2020-08-03 13:45:08 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 100}
+
+    VALID_CATEGORIES = ["Fiction","Non-Fiction"]
+    validates :category, inclusion: {in: VALID_CATEGORIES}
+
 end
