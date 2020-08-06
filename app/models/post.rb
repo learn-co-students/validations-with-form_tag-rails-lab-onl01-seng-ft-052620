154a664e6fadcd600987bff84fc5d0f2c6cbf710
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-06 16:43:52 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, length: { minimum: 2 }
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+   message: "%{value} is not a valid category" }
 end
